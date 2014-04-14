512c0bd66d424ef2cb958e247122b9422a8a4b36
Steven Occhipinti <dev@stevenocchipinti.com>
2014-04-14 22:55:57 +1000

    Initial commit

---
+++ templates/yoo_nano/js/template.js
@@ -0,0 +1,30 @@
+/* Copyright (C) 2007 - 2011 YOOtheme GmbH, YOOtheme Proprietary Use License (http://www.yootheme.com/license) */
+
+(function($){
+
+	$(document).bind('ready', function() {
+
+		// Accordion menu
+		$('.menu-sidebar').accordionMenu({ mode:'slide' });
+
+		// Dropdown menu
+		$('#menu').dropdownMenu({ mode: 'slide', dropdownSelector: 'div.dropdown'});
+
+		// Smoothscroller
+		$('a[href="#page"]').smoothScroller({ duration: 500 });
+
+		// Fix Browser Rounding
+		$('.grid-block').matchWidth('.grid-h');
+
+		// Match height of div tags
+		$('#top-a .grid-h').matchHeight('.deepest');
+		$('#top-b .grid-h').matchHeight('.deepest');
+		$('#bottom-a .grid-h').matchHeight('.deepest');
+		$('#bottom-b .grid-h').matchHeight('.deepest');
+		$('#innertop .grid-h').matchHeight('.deepest');
+		$('#innerbottom .grid-h').matchHeight('.deepest');
+		$('#maininner, #sidebar-a, #sidebar-b').matchHeight();
+		
+	});
+	
+})(jQuery);
